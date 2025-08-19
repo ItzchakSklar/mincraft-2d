888c47d711e4c72e29a8b045d00b08e5d063cbf1
itzchak <s0527120124@gmail.com>
2025-08-19 17:59:34 +0300

    Remove grass tile definition from TILES configuration

--- js/tiles.js
+++ js/tiles.js
@@ -8,7 +8,6 @@
  */
 export const TILES = {
   sky:   { placeable: false },
-  grass: { placeable: true },
   dirt:  { placeable: true, tool: "shovel" },
   tree:  { placeable: true, tool: "axe" },
   rock:  { placeable: true, tool: "pickaxe" }
