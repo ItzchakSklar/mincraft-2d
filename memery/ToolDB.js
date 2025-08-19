a132e03ce0290628e8b547332fe17ea429196f24
itzchak <s0527120124@gmail.com>
2025-08-19 17:04:41 +0300

    Add InventoryManager, GameState, and ToolDB classes for game inventory management

---
+++ memery/ToolDB.js
@@ -0,0 +1,9 @@
+export const TILES = {
+  sky:   { placeable: false },
+  grass: { placeable: true },
+  dirt:  { placeable: true },
+  tree:  { placeable: true, tool: "axe" },
+  rock:  { placeable: true, tool: "pickaxe" }
+};
+
+export const TOOLS = ["axe", "pickaxe", "shovel", "place"];
