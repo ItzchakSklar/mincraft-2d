4530f6249a407ee9f15c8e74d78eeaf8213b10f3
itzchak <s0527120124@gmail.com>
2025-08-19 17:16:14 +0300

    Add initial game files and remove obsolete inventory management classes

--- memery/ToolDB.js
+++
@@ -1,9 +0,0 @@
-export const TILES = {
-  sky:   { placeable: false },
-  grass: { placeable: true },
-  dirt:  { placeable: true },
-  tree:  { placeable: true, tool: "axe" },
-  rock:  { placeable: true, tool: "pickaxe" }
-};
-
-export const TOOLS = ["axe", "pickaxe", "shovel", "place"];
