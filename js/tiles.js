6defd2ee3e64739fff4dd0803364277bae71991e
BeatDiaz <beatdiazmusic@gmail.com>
2025-08-20 12:32:19 +0300

    update tiles.js

--- js/tiles.js
+++ js/tiles.js
@@ -1,8 +1,5 @@
 const tilesContainer = document.getElementById("tilesGrid");
 
-const columns = 100;
-const rows = 50;
-
 const tileType = {
     sky: "sky",
     grass: "grass",
@@ -16,19 +13,26 @@ const breakable = {
     false: "unbreakable"
 }
 
+const horizonLine = 8;
+const dirtBorder = 27;
+const stoneBorder = 48;
+
+const columns = 100;
+const rows = 50;
+
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < columns; col++) {
         const tile = document.createElement("div");
         tile.id = `tile:${row * 100 + col}|`; //Setting a unique ID for each tile according to its row
         tile.classList.add("tile");
 
-        if (row < 6) {
+        if (row < horizonLine) {
             tile.classList.add(tileType.sky, breakable.false);
-        } else if (row === 6) {
+        } else if (row === horizonLine) {
             tile.classList.add(tileType.sky, tileType.grass, breakable.true);
-        } else if (row < 27) {
+        } else if (row < dirtBorder) {
             tile.classList.add(tileType.sky, tileType.dirt, breakable.true);
-        } else if (row < 48) {
+        } else if (row < stoneBorder) {
             tile.classList.add(tileType.sky, tileType.stone, breakable.true);
         } else {
             tile.classList.add(tileType.bedrock, breakable.false);
